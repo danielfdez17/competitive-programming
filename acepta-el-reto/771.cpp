d2a9b508068b981de9f4007dbaabd02d6a712ffb
danielfdez17 <daniel17dev@gmail.com>
2024-12-31 09:09:20 +0100

    771 CORRECT

---
+++ acepta-el-reto/771.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        int a;
+        cin >> a;
+        cout << a * 4 << "\n";
+    }
+    return 0;
+}
